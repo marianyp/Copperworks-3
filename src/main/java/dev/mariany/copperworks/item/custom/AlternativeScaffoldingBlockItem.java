e03e8dd25d15d52867ddce0f26eb1f79c170fb54
Mariany Pinkston <marianypinkston@gmail.com>
2026-01-06 02:19:21 -0500

    Improve Copper Scaffolding placement and fix not placing when sneaking while looking down

--- src/main/java/dev/mariany/copperworks/item/custom/AlternativeScaffoldingBlockItem.java
+++ src/main/java/dev/mariany/copperworks/item/custom/AlternativeScaffoldingBlockItem.java
@@ -1,8 +1,6 @@
 package dev.mariany.copperworks.item.custom;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
@@ -10,15 +8,12 @@ import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+
+import java.util.function.DoubleFunction;
 
 public class AlternativeScaffoldingBlockItem extends BlockItem {
     public AlternativeScaffoldingBlockItem(Block block, Settings settings) {
@@ -32,172 +27,63 @@ public class AlternativeScaffoldingBlockItem extends BlockItem {
     @Override
     public ActionResult use(World world, PlayerEntity player, Hand hand) {
         boolean isClose = player.isSneaking();
-        double distance = isClose ? 1.5 : player.getAttributeValue(EntityAttributes.BLOCK_INTERACTION_RANGE);
+        double blockInteractionRange = player.getAttributeValue(EntityAttributes.BLOCK_INTERACTION_RANGE);
+        double startDistance = isClose ? 1.5 : blockInteractionRange;
 
-        if (hand.equals(Hand.OFF_HAND) && player.getMainHandStack().isOf(this)) {
+        if (hand == Hand.OFF_HAND && player.getMainHandStack().isOf(this)) {
             return ActionResult.PASS;
         }
 
-        BlockPos pos = getPlacePositionWithRaycast(world, player, hand, distance);
-
-        if (pos == null) {
-            return ActionResult.FAIL;
-        }
-
         if (!world.isClient()) {
-            this.place(createItemPlacementContext(player, hand, pos));
+            final double step = 0.5;
+
+            DoubleFunction<ActionResult> tryPlaceAt = (distance) -> {
+                BlockPos pos = getPlacePosition(player, distance);
+                return this.place(createItemPlacementContext(player, hand, pos));
+            };
+
+            for (double distance = startDistance; distance > 0; distance -= step) {
+                ActionResult result = tryPlaceAt.apply(distance);
+
+                if (result.isAccepted()) {
+                    return result;
+                }
+            }
+
+            for (double distance = 0; distance <= blockInteractionRange; distance += step) {
+                ActionResult result = tryPlaceAt.apply(distance);
+
+                if (result.isAccepted()) {
+                    return result;
+                }
+            }
+
+            return ActionResult.FAIL;
         }
 
         return ActionResult.SUCCESS;
     }
 
+    private static BlockPos getPlacePosition(PlayerEntity player, double distance) {
+        Vec3d rotationVec = player.getRotationVec(1);
+
+        double x = player.getX() + rotationVec.x * distance;
+        double y = player.getEyeY() + rotationVec.y * distance;
+        double z = player.getZ() + rotationVec.z * distance;
+
+        return new BlockPos((int) Math.floor(x), (int) Math.floor(y), (int) Math.floor(z));
+    }
+
     private static ItemPlacementContext createItemPlacementContext(PlayerEntity player, Hand hand, BlockPos pos) {
+        Direction side = Direction.getFacing(player.getRotationVec(1)).getOpposite();
+
         BlockHitResult blockHitResult = new BlockHitResult(
                 Vec3d.ofBottomCenter(pos),
-                Direction.DOWN,
+                side,
                 pos,
                 false
         );
 
         return new ItemPlacementContext(player, hand, player.getStackInHand(hand), blockHitResult);
-    }
-
-    @Nullable
-    private BlockPos getPlacePositionWithRaycast(World world, PlayerEntity player, Hand hand, double maxDistance) {
-        if (isEntityBlocking(world, player, maxDistance)) {
-            return null;
-        }
-
-        double targetedFrameDistance = getTargetedDistance(world, player, maxDistance);
-        double remainingDistance = targetedFrameDistance > -1 ? targetedFrameDistance : maxDistance;
-
-        Vec3d startPos = player.getEyePos();
-        Vec3d direction = player.getRotationVec(1.0F);
-
-        while (remainingDistance > 0) {
-            Vec3d endPos = startPos.add(direction.multiply(remainingDistance));
-
-            RaycastContext context = new RaycastContext(
-                    startPos,
-                    endPos,
-                    RaycastContext.ShapeType.COLLIDER,
-                    RaycastContext.FluidHandling.NONE,
-                    player
-            );
-
-            BlockHitResult hitResult = world.raycast(context);
-
-            BlockPos hitPos = hitResult.getBlockPos();
-            BlockState hitBlockState = world.getBlockState(hitPos);
-            Direction hitSide = hitResult.getSide();
-
-            if (hitResult.getType() == HitResult.Type.MISS) {
-                if (!hitBlockState.isOf(this.getBlock())) {
-                    BlockPos placePos = BlockPos.ofFloored(endPos);
-
-                    if (this.canPlace(createItemPlacementContext(player, hand, placePos))) {
-                        return placePos;
-                    }
-                }
-            }
-
-            remainingDistance -= 0.5;
-
-            if (remainingDistance <= 0) {
-                return null;
-            }
-
-            if (!hitBlockState.isOf(this.getBlock())) {
-                BlockPos placePos = hitPos.offset(hitSide);
-
-                if (this.canPlace(createItemPlacementContext(player, hand, placePos))) {
-                    return placePos;
-                }
-            }
-        }
-
-        return null;
-    }
-
-    private double getTargetedDistance(World world, PlayerEntity player, double maxDistance) {
-        Double result = raycastIncrementally(
-                world,
-                player,
-                maxDistance,
-                (blockHitResult, distance) -> {
-                    if (blockHitResult.getType() == HitResult.Type.MISS) {
-                        BlockPos pos = blockHitResult.getBlockPos();
-                        BlockState state = world.getBlockState(pos);
-
-                        if (state.isOf(this.getBlock())) {
-                            return distance;
-                        }
-                    }
-
-                    return null;
-                }
-        );
-
-        return result != null ? result : -1;
-    }
-
-    private static boolean isEntityBlocking(World world, PlayerEntity player, double maxDistance) {
-        Boolean result = raycastIncrementally(
-                world,
-                player,
-                maxDistance,
-                (blockHitResult, distance) -> {
-                    if (isEntityOccupyingSpace(world, blockHitResult.getBlockPos(), player)) {
-                        return true;
-                    }
-
-                    return null;
-                }
-        );
-
-        return result != null && result;
-    }
-
-    private static boolean isEntityOccupyingSpace(World world, @NotNull BlockPos pos, @Nullable PlayerEntity player) {
-        return world.getNonSpectatingEntities(Entity.class, Box.from(Vec3d.of(pos)))
-                    .stream()
-                    .anyMatch(entity -> !entity.equals(player));
-    }
-
-    private static <T> T raycastIncrementally(
-            World world,
-            PlayerEntity player,
-            double maxDistance,
-            RayPredicate<T> rayPredicate
-    ) {
-        Vec3d startPos = player.getEyePos();
-        Vec3d direction = player.getRotationVec(1);
-
-        for (int distance = 0; distance < maxDistance; distance += 1) {
-            Vec3d endPos = startPos.add(direction.multiply(distance));
-
-            RaycastContext context = new RaycastContext(
-                    startPos,
-                    endPos,
-                    RaycastContext.ShapeType.COLLIDER,
-                    RaycastContext.FluidHandling.NONE,
-                    player
-            );
-
-            BlockHitResult blockHitResult = world.raycast(context);
-
-            T out = rayPredicate.test(blockHitResult, distance);
-
-            if (out != null) {
-                return out;
-            }
-        }
-
-        return null;
-    }
-
-    @FunctionalInterface
-    private interface RayPredicate<T> {
-        T test(BlockHitResult hit, double distance);
     }
 }
