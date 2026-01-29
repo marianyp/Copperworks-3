e88e73ad581c1840a5d06b2bacf3bcf6dec50135
Mariany Pinkston <marianypinkston@gmail.com>
2026-01-29 17:12:01 -0500

    Code cleanup

--- src/main/java/dev/mariany/copperworks/block/custom/sensor/SensorBlockEntity.java
+++ src/main/java/dev/mariany/copperworks/block/custom/sensor/SensorBlockEntity.java
@@ -1,11 +1,28 @@
 package dev.mariany.copperworks.block.custom.sensor;
 
 import dev.mariany.copperworks.block.CWBlockEntities;
+import dev.mariany.copperworks.sound.CWSoundEvents;
 import net.minecraft.block.BlockState;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 
-public class SensorBlockEntity extends AbstractSensorBlockEntity{
+public class SensorBlockEntity extends AbstractSensorBlockEntity {
     public SensorBlockEntity(BlockPos pos, BlockState state) {
         super(CWBlockEntities.SENSOR, pos, state, 16);
+    }
+
+    @Override
+    protected void playSound(ServerWorld serverWorld, BlockPos pos) {
+        serverWorld.playSound(
+                null,
+                pos.getX(),
+                pos.getY(),
+                pos.getZ(),
+                CWSoundEvents.BLOCK_SENSOR_INTERACT,
+                SoundCategory.BLOCKS,
+                0.5F,
+                (float) (1.6 - (this.range - 1) * 0.1)
+        );
     }
 }
